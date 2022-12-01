edd9e9947bb2fda70b6c2c6b1501b6bedb7c19b6
Mirko Saugo <himynameismirko@gmail.com>
2022-12-02 00:29:55 +0100

    feat: :art: replaced Image plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,20 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-import image from "@astrojs/image";
+import { astroImageTools } from "astro-imagetools";
 
 // import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  // experimental: {
+  //   integrations: true,
+  // },
   integrations: [tailwind({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
     config: {
       applyBaseStyles: false
     }
-  }), image()]
+  }), astroImageTools]
 });
