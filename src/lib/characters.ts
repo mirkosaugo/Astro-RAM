83918dca961c216c19aec0a832e61e40b8121687
Mirko Saugo <himynameismirko@gmail.com>
2022-12-05 17:04:09 +0100

    docs: :memo: edited package.json and added env var

--- src/lib/characters.ts
+++ src/lib/characters.ts
@@ -1,10 +1,12 @@
+const API_URL = import.meta.env.PUBLIC_RICKANDMORTYAPI;
+
 import { ICharacter, IAllCharacters } from '../types/characters';
 
 export default async function getCharacters(): Promise<IAllCharacters> {
-  const mainCharactersResponse = await fetch('https://rickandmortyapi.com/api/character/1,2,3,4,5');
+  const mainCharactersResponse = await fetch(`${API_URL}/character/1,2,3,4,5`);
   const mainCharacters: ICharacter[] = await mainCharactersResponse.json();
 
-  const response = await fetch('https://rickandmortyapi.com/api/character/?species=alien');
+  const response = await fetch(`${API_URL}/character/?species=alien`);
   const { results } = await  response.json();
   const aliensCharacters: ICharacter[] = results.slice(0, 5);
 
