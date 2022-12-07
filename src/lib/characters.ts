a8e2b03accf632edaa1384028eb07b3fe0a0c2d5
Mirko Saugo <himynameismirko@gmail.com>
2022-12-07 14:12:22 +0100

    feat: :dizzy: prevNext character implementation

--- src/lib/characters.ts
+++ src/lib/characters.ts
@@ -3,15 +3,23 @@ const API_URL = import.meta.env.PUBLIC_RICKANDMORTYAPI;
 import { ICharacter, IAllCharacters } from '../types/characters';
 
 export default async function getCharacters(): Promise<IAllCharacters> {
-  const mainCharactersResponse = await fetch(`${API_URL}/character/1,2,3,4,5`);
-  const mainCharacters: ICharacter[] = await mainCharactersResponse.json();
+  try {
+    const mainCharactersResponse = await fetch(`${API_URL}/character/1,2,3,4,5`);
+    const mainCharacters: ICharacter[] = await mainCharactersResponse.json();
 
-  const response = await fetch(`${API_URL}/character/?species=alien`);
-  const { results } = await  response.json();
-  const aliensCharacters: ICharacter[] = results.slice(0, 5);
+    const response = await fetch(`${API_URL}/character/?species=alien`);
+    const { results } = await  response.json();
+    const aliensCharacters: ICharacter[] = results.slice(0, 5);
 
-  return {
-    mainCharacters,
-    aliensCharacters
-  };
-}
+    return {
+      mainCharacters,
+      aliensCharacters
+    };
+  } catch (error) {
+    return {
+      mainCharacters: [],
+      aliensCharacters: []
+    };
+  }
+};
+
